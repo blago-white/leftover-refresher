eacf26e210e7518b3b46b34ed8e048c86abd2948
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-01-30 11:05:08 +0300

    Add help text

--- main.py
+++ main.py
@@ -8,13 +8,40 @@ from src import run_refreshing, refresh_tracked_articles_list
 from src.config import config, settings
 
 
+_HELP_STRING = """
+Follow creator - https://github.com/blago-white
+
+This is a script for synchronizing product balances on different platforms, 
+saving hundreds of hours spent on synchronizing data between platforms
+
+Example of run script -----------------------------------
+
+./main.py refresh --articles
+
+Arguments -----------------------------------------------
+
+help           Show this help info
+
+refresh        Refresh data, stocks or list of tracked articles 
+               depending on the options
+
+Options -------------------------------------------------
+
+
+--articles     Refresh only list of tracked articles
+--stocks       Refresh stocks of tracked articles
+
+"""
+
+
 def main():
-    if (len(sys.argv) < 3
-            or not sys.argv[1] == "refresh"
-            or sys.argv[2] == "help"):
+    if (len(sys.argv) < 2) or (sys.argv[1] == "help"):
+        print(_HELP_STRING)
+
+    if not sys.argv[1] == "refresh":
         return
 
-    if sys.argv[2] == "--stocks":
+    elif sys.argv[2] == "--stocks":
         asyncio.run(run_refreshing(
             credentals=config.load_config(
                 path=settings.ConfigSettings.CONFIG_FILE_PATH
